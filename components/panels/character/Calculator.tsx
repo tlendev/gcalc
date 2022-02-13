99fa724fc60658fb241e934cbe4af27c23f57eb8
tlendev <oxyaego98@gmail.com>
2022-02-14 00:03:23 +0100

    Start work on Calculator board component

---
+++ components/panels/character/Calculator.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import calcStyles from './calculator.module.css';
+
+const Calculator = () => {
+	return <div className={calcStyles.container}>Calculator</div>;
+};
+
+export default Calculator;
