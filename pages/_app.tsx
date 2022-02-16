40171d1fb6c1dbfdad782cf41644205442289361
tlendev <oxyaego98@gmail.com>
2022-02-17 00:23:43 +0100

    Delete unused paralaxroot

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-import { ParallaxProvider } from 'react-scroll-parallax';
 import { RecoilRoot } from 'recoil';
 import '../styles/globals.css';
 import '../styles/normalize.css';
@@ -7,9 +6,7 @@ import '../styles/normalize.css';
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<RecoilRoot>
-			<ParallaxProvider>
-				<Component {...pageProps} />
-			</ParallaxProvider>
+			<Component {...pageProps} />
 		</RecoilRoot>
 	);
 }
